02a688bd32947c3a5c986bde50e83fb8328d8a2b
ParkJaeRim <dbahck200@gmail.com>
2022-01-02 15:54:25 +0900

    feat: 프론트엔드 프로젝트 업로드

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+
+  created() {
+    axios
+      .get('http://localhost:8080/test')
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  },
+};
+</script>
